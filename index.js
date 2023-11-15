0727c87e2ae81b1416636fa8bffb19ef5eff6bad
Angel Gasca <cesargasca083@gmail.com>
2023-11-15 11:54:39 -0600

    feat: acepta el atributo multiple
    Se hacen algunas refactorizaciones de codigo para mejorar la legibilidad y se añade la propiedad que indica si es multiple o no, solo para drop, para click se sigue trabajando

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var dropContainersArray = document.getElementsByClassName("dragDropFiles");
 var dropContainers = document.querySelectorAll(".dragDropFiles");
+var isMultiple = false
 
 dropContainers.forEach(dropContainer => {
   // Se inserta la información de las indicacionese
-  var pTag = document.createElement("p")
-  pTag.innerHTML = "<strong>Arrastra y suelta <br> o <br>da click para subir los archivos</strong>"
-  dropContainer.appendChild(pTag)
+  var mainContainer = document.createElement("div")
+  mainContainer.classList = "imagePreview__main" 
+  mainContainer.appendChild(createPTag())
 
   // Se inserta un "input" para agregar las imagenes
   var inputFile = document.createElement("input")
@@ -16,37 +16,19 @@ dropContainers.forEach(dropContainer => {
   inputFile.accept = ['image/jpeg','image/jpg','image/png','image/gif','image/webp'];
   dropContainer.removeAttribute("id")
   inputFile.setAttribute("hidden", "")
-  dropContainer.appendChild(inputFile)
+  if (dropContainer.getAttribute("multiple") == "") {
+    inputFile.setAttribute("multiple", "")
+    isMultiple = true
+  }
+  mainContainer.appendChild(inputFile)
+  dropContainer.appendChild(mainContainer)
 });
 
-document.addEventListener('click', (e) => {
-  e.stopPropagation();
-  var closePreview = e.target.matches(".closePreview")
-  var dragDropElement = e.target.closest(".dragDropFiles")
-  if (closePreview) {
-    dragDropElement.innerHTML = ""
-    var pTag = document.createElement("p")
-    pTag.innerHTML = "<strong>Arrastra y suelta <br> o <br>da click para subir los archivos</strong>"
-    dragDropElement.appendChild(pTag)
-
-    // Se inserta un "input" para agregar las imagenes
-    var inputFile = document.createElement("input")
-    inputFile.type = "file"
-    inputFile.name = dragDropElement.getAttribute("name")
-    dragDropElement.removeAttribute("name")
-    inputFile.id = dragDropElement.getAttribute("id")
-    inputFile.accept = ['image/jpeg','image/jpg','image/png','image/gif','image/webp'];
-    dragDropElement.removeAttribute("id")
-    inputFile.setAttribute("hidden", "")
-    dragDropElement.appendChild(inputFile)
-    return
-  }
-  if (dragDropElement) {
-    
-    var input = dragDropElement.querySelector("input[type=file]");
-    input.click()
-  }
-})
+function createPTag() {
+  var pTag = document.createElement("p")
+  pTag.innerHTML = "<strong>Arrastra y suelta <br> o <br>da click para subir los archivos</strong>"
+  return pTag
+}
 
 document.addEventListener('dragover', (e) => {
   e.preventDefault(); // Evitar el comportamiento predeterminado del navegador
@@ -54,7 +36,7 @@ document.addEventListener('dragover', (e) => {
   if (dragDropElement) {
     var files = dragDropElement.querySelector("input[type=file]").files
     if (files.length == 0) {
-      var pTag = dragDropElement.firstElementChild;
+      var pTag = dragDropElement.querySelector("p")
       pTag.innerHTML = "<strong>Suelta el archivo</strong>";
     }
     dragDropElement.classList.add("active");
@@ -66,42 +48,80 @@ document.addEventListener('dragleave', (e) => {
   var dragDropElement = e.target.closest(".dragDropFiles");
   if (dragDropElement) {
     var files = dragDropElement.querySelector("input[type=file]").files
+    var pTag = dragDropElement.querySelector("p")
     if (files.length == 0) {
-      var files = dragDropElement.querySelector("input[type=file]").files
-      var pTag = dragDropElement.firstElementChild;
       pTag.innerHTML = "<strong>Arrastra y suelta <br> o <br>da click para subir los archivos</strong>";
     }
     dragDropElement.classList.remove("active");
   }
 });
+
 document.addEventListener('drop', (e) => {
   e.preventDefault();
   var dragDropElement = e.target.closest(".dragDropFiles");
   if (dragDropElement) {
     dragDropElement.classList.remove("active");
-    files = e.dataTransfer.files
-    dragDropElement.querySelector("input[type=file]").files = files
-    showFiles(files, dragDropElement)
-  }
-});
-document.addEventListener('change', (e) => {
-  var dragDropElement = e.target.closest(".dragDropFiles");
-  if (dragDropElement) {
-    var files = dragDropElement.querySelector("input[type=file]").files
-    showFiles(files, dragDropElement)
+
+    var input = dragDropElement.querySelector("input[type=file]") 
+    var currentFiles = e.dataTransfer.files
+    addFiles(input, currentFiles, "drop")
+    
+    
+    console.log(input.files)
+    showFiles(currentFiles, dragDropElement);
   }
 });
 
-function showFiles(files,dragDropElement) {
+document.addEventListener('click', (e) => {
+  e.stopPropagation();
+  
+  var closePreview = e.target.matches(".closePreview")
+  var dragDropElement = e.target.closest(".dragDropFiles")
+
+  if (closePreview) {
+    var input = dragDropElement.querySelector("input[type=file]");
+    var mainPreview = dragDropElement.querySelector("div.imagePreview__main")
+    const child = e.target.closest("div.imagePreview__item")
+    child.remove()
+    // input.value = ""
+    // console.log(input)
+    if (input.files == 0) {
+      mainPreview.appendChild(createPTag())
+    }
+    return
+  }
+  if (dragDropElement) {
+    var input = dragDropElement.querySelector("input[type=file]");
+    input.click()
+  }
+});
+
+document.addEventListener('change', (e) => {
+  var dragDropElement = e.target.closest(".dragDropFiles");
+  if (dragDropElement) {
+    var input = dragDropElement.querySelector("input[type=file]")
+    var currentFiles = new DataTransfer()
+    console.log("input.files")
+    console.log(input.files)
+    addFiles(input, currentFiles.file, "click")
+    console.log(input.files)
+    // showFiles(input, dragDropElement)
+    // console.log(files)
+  }
+});
+
+function showFiles(files ,dragDropElement) {
   if (files.length == 0) {
     return
   }
-  if (files.length === undefined || files.length > 1) {
-    alert("Solo se puede procesar una imagen")
+  if (!isMultiple && files.length > 1) {
+    alert("Solo se acepta un archivo a la vez")
     return
   }
-  const file = files[0]
-  processFile(file, dragDropElement)
+  for (let i = 0; i < files.length; i++) {
+    processFile( files[i], dragDropElement)
+  }
+  return
 }
 
 function processFile(file, dragDropElement) {
@@ -110,28 +130,50 @@ function processFile(file, dragDropElement) {
   if (!validExtensions.includes(fileExtension)) {
     alert('No es un archivo válido')
     return
-  } 
+  }
+  if (dragDropElement.querySelector('p') != undefined) {
+    dragDropElement.querySelector('p').remove();
+  }
+  
   // Crear un objeto de tipo FileReader
   const fileReader = new FileReader();
   fileReader.readAsDataURL(file);
-
   fileReader.addEventListener('load', () => {
+    var mainPreview = dragDropElement.querySelector('div.imagePreview__main')
     const fileUrl = fileReader.result;
-    //   `;
     const image = `
         <span class="closePreview"></span>
         <img src="${fileUrl}" alt="${file.name}">
         <span name="texto nombre">${file.name}</span>
-      `;
-    const input = dragDropElement.querySelector("input[type=file]")
-    if (dragDropElement.querySelector('p') != undefined) {
-      dragDropElement.querySelector('p').remove();
+    `;
+    var itemPreview = document.createElement("div")
+    itemPreview.classList = "imagePreview__item"
+    if (!isMultiple && mainPreview.querySelectorAll('div.imagePreview__item').length > 0) {
+      itemPreview = mainPreview.querySelector('div.imagePreview__item');
+      itemPreview.innerHTML = "";
     }
-    if (dragDropElement.querySelector('div') != undefined) {
-      dragDropElement.querySelector('div').remove();
-    }
-    var preview = document.createElement("div")
-    preview.innerHTML = image
-    dragDropElement.insertBefore(preview, input);
+    itemPreview.innerHTML += image;
+    mainPreview.appendChild(itemPreview)
   });
+}
+
+function addFiles(input, currentFiles, action) {
+  if (!isMultiple) {
+    input.files = currentFiles
+    return
+  }
+  var dataTransfer = new DataTransfer();
+  if (input.files.length == 0) 
+    input.files = currentFiles
+  else {
+    console.log("pasa directo desde el click")
+    for (const file of input.files) {
+      dataTransfer.items.add(file)
+    }
+    for (const currentFile of currentFiles) {
+      dataTransfer.items.add(currentFile)
+    }
+    input.files = dataTransfer.files
+  }
+
 }
